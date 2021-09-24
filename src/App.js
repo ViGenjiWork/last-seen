aa3f1185b8b8fd0a8d2ff2e49219d43765f893a3
Mikhail Zharkov <zharkovma00@mail.ru>
2021-09-25 04:44:53 +0700

    fixed img routes

--- src/App.js
+++ src/App.js
@@ -2,87 +2,84 @@ import React from 'react'
 
 import './App.scss';
 
-import { Header } from './components/Header/Header';
-import { Home } from './pages/Home';
+import {Header} from './components/Header/Header';
+import {Home} from './pages/Home';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import { Product } from './pages/Product';
-import { Cart } from './components/Cart/Cart';
+import {Product} from './pages/Product';
+import {Cart} from './components/Cart/Cart';
 import axios from 'axios';
-import { Context } from './context';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCartItems } from './redux/actions/cartItems';
+import {Context} from './context';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchCartItems} from './redux/actions/cartItems';
 
 function App() {
 
-  const [cartOpened, setCartOpened] = React.useState(false)
-  const [cartItems, setCartItems] = React.useState([])
+    const [cartOpened, setCartOpened] = React.useState(false)
+    const [cartItems, setCartItems] = React.useState([])
 
-  const dispatch = useDispatch()
-  const cartItem = useSelector(({ cartItems }) => cartItems.items)
+    const dispatch = useDispatch()
+    const cartItem = useSelector(({cartItems}) => cartItems.items)
 
-  React.useEffect(() => {
+    React.useEffect(() => {
 
-    // (async () => {
-    //   try {
-    //     const [cartResponse] = await Promise.all([
-    //       axios.get('https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems')
-    //     ])
-    //     setCartItems(cartResponse.data)
-    //   } catch (e) {
-    //     console.log(e)
-    //   }
-    // })()
-    dispatch(fetchCartItems)
+        // (async () => {
+        //   try {
+        //     const [cartResponse] = await Promise.all([
+        //       axios.get('https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems')
+        //     ])
+        //     setCartItems(cartResponse.data)
+        //   } catch (e) {
+        //     console.log(e)
+        //   }
+        // })()
+        dispatch(fetchCartItems)
 
-  }, [])
+    }, [])
 
-  const removeCartItem = (id) => {
-    try {
-      axios.delete(`https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems/${id}`)
-      setCartItems(prev => prev.filter(item => item.id !== id))
+    const removeCartItem = (id) => {
+        try {
+            axios.delete(`https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems/${id}`)
+            setCartItems(prev => prev.filter(item => item.id !== id))
+        } catch (e) {
+            console.log(e);
+        }
     }
-    catch (e) {
-      console.log(e);
+
+    const addCartItem = (cartItem) => {
+        try {
+            axios.post('https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems', cartItem)
+            setCartItems(prev => prev.filter(item => item.id !== cartItem.id))
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
 
-  const addCartItem = (cartItem) => {
-    try {
-      axios.post('https://6112f8ba89c6d00017ac0605.mockapi.io/cartItems', cartItem)
-      setCartItems(prev => prev.filter(item => item.id !== cartItem.id))
-    } catch (e) {
-      console.log(e);
-    }
-  }
+    return (
+        <Context.Provider value={{
+            addCartItem
+        }}>
+            <div className="app">
+                <Header
+                    onCLickCart={() => setCartOpened(!cartOpened)}
+                />
+                <Cart
+                    items={cartItems}
+                    opened={cartOpened}
+                    onCloseCart={() => setCartOpened(!cartOpened)}
+                    removeCartItem={removeCartItem}
+                />
 
-  return (
-    <Context.Provider value={{
-      addCartItem
-    }}>
-      <div className="app">
-        <Header
-          onCLickCart={() => setCartOpened(!cartOpened)}
-        />
-        <Cart
-          items={cartItems}
-          opened={cartOpened}
-          onCloseCart={() => setCartOpened(!cartOpened)}
-          removeCartItem={removeCartItem}
-        />
-        <Switch>
-            <Redirect from='/last-seen/' to="/last-seen" />
-              <Route exact path='/last-seen/'>
-                <Home />
-              </Route>
-        </Switch>
+                <Route path='/last-seen/'>
+                    <Home/>
+                </Route>
 
-        <Route path={`/products/`} >
-          <Product />
-        </Route>
-      </div>
-    </Context.Provider>
+                <Route path={`/products/`}>
+                    <Product/>
+                </Route>
+            </div>
+        </Context.Provider>
 
-  );
+    );
 }
 
 export default App;
